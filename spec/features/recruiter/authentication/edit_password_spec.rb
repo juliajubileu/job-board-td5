b0f5a5bbe8ae0f3db44058616c2a7f150f47ab93
Julia Jubileu <juliajubileu@gmail.com>
2021-03-29 18:02:37 -0300

    Recrutador edita senha

--- spec/features/recruiter/authentication/edit_password_spec.rb
+++ spec/features/recruiter/authentication/edit_password_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-xfeature 'Recruiter edits password' do
+feature 'Recruiter edits password' do
   scenario 'successfully' do
     # Arrange
     company = create(:company, name: 'Treina Dev',
@@ -13,43 +13,16 @@ xfeature 'Recruiter edits password' do
     # Act
     login_as recruiter, scope: :recruiter
     visit root_url
-    click_on 'Painel do recrutador'
-    click_on 'Atualizar senha'
-    within('form') do
-      fill_in 'Senha anterior', with: '124567'
-      fill_in 'Nova senha', with: '654321'
-      fill_in 'Confirme a nova senha', with: '654321'
-    end
+    click_on recruiter.email
+    click_on 'Atualizar conta'
+    fill_in 'Senha', with: '654321'
+    fill_in 'Confirmação de senha', with: '654321'
+    fill_in 'Senha atual', with: '124567'
+    click_on 'Atualizar'
 
     # Assert
-    expect(page).to have_content('Senha atualizada com sucesso')
+    expect(page).to have_content('Sua conta foi atualizada com sucesso')
     expect(page).to have_content('rh@treinadev.com.br')
-    expect(page).to have_link('Sair')
-  end
-
-  scenario 'and new password can not be blank' do
-    # Arrange
-    company = create(:company, name: 'Treina Dev',
-                               address: 'Alameda Santos, 1293',
-                               cnpj: '11.222.333/0000-44',
-                               website: 'www.treinadev.com.br',
-                               domain: 'treinadev.com.br')
-    recruiter = create(:recruiter, company: company)
-
-    # Act
-    login_as recruiter, scope: :recruiter
-    visit root_url
-    click_on 'Painel do recrutador'
-    click_on 'Atualizar senha'
-    within('form') do
-      fill_in 'Senha anterior', with: '124567'
-      fill_in 'Nova senha', with: ''
-      fill_in 'Confirme a nova senha', with: ''
-    end
-
-    # Assert
-    expect(page).to have_content('Não foi possível atualizar senha')
-    expect(page).to have_content('Senha não pode ficar em branco')
     expect(page).to have_link('Sair')
   end
 
@@ -65,17 +38,16 @@ xfeature 'Recruiter edits password' do
     # Act
     login_as recruiter, scope: :recruiter
     visit root_url
-    click_on 'Painel do recrutador'
-    click_on 'Atualizar senha'
-    within('form') do
-      fill_in 'Senha anterior', with: '124567'
-      fill_in 'Nova senha', with: '123456'
-      fill_in 'Confirme a nova senha', with: '234567'
-    end
+    click_on recruiter.email
+    click_on 'Atualizar conta'
+    fill_in 'Confirmação de senha', with: '234567'
+    fill_in 'Senha', with: '123456'
+    fill_in 'Senha atual', with: '124567'
+    click_on 'Atualizar'
 
     # Assert
-    expect(page).to have_content('Não foi possível atualizar senha')
-    expect(page).to have_content('Confirmação de senha não é igual a senha')
+    expect(page).to have_content('Não foi possível salvar recrutador')
+    expect(page).to have_content('Confirmação de senha não é igual a Senha')
     expect(page).to have_link('Sair')
   end
 end
